223525498a8c7cea93c5a5b405101a96c72bd65c
Hyundong <gusehd1113@gmail.com>
2021-08-07 15:21:17 +0900

    commit 3

---
+++ explain.py
@@ -0,0 +1,8 @@
+def ICT():
+    return """>>ICT란? 정보통신기술을 뜻하며 Information & Communication Technology의 약자이다. """
+def ITU():
+    return """>>ITU란? 유엔소속의 국제전기통신연합을 뜻하며 International Telecommunication Union의 약자이다. ICT와 관련된 이슈를 다룬다."""
+def IMD():
+    return """>>IMD란? 국제경영개발원을 뜻하는 민간 기관이다. International Institute for Management Development의 약자이다. 매년 국가경쟁력지수를 발표한다."""
+def UNCTAD():
+    return """>>UNCTAD란? 유엔소속의 유엔무역개발기구를 뜻하며 여러 지수를 종합하여 B2C 전자상거래 지수를 측정하여 국가 간 비교를 한다."""
